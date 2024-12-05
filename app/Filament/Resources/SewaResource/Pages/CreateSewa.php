64d3152866026e1f738d6dc21b95689dfb606b09
Richardooktavianus <zeusmati1234@gmail.com>
2024-12-05 12:16:03 +0700

    beres om

--- app/Filament/Resources/SewaResource/Pages/CreateSewa.php
+++ app/Filament/Resources/SewaResource/Pages/CreateSewa.php
@@ -3,10 +3,18 @@
 namespace App\Filament\Resources\SewaResource\Pages;
 
 use App\Filament\Resources\SewaResource;
+use App\Models\Kamar;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateSewa extends CreateRecord
 {
     protected static string $resource = SewaResource::class;
+
+    protected function afterCreate(): void
+    {
+        $kamar = Kamar::find($this->record->kamar_id);
+        $kamar->update(['status' => 'Tidak Tersedia']);
+
+    }
 }
